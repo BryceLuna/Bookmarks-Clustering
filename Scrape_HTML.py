3df11481be5f838c1b69ad8213cd3ae20b298ad7
bryceluna <bryce.luna@gmail.com>
2017-03-08 18:34:50 -0700

    parallel links fn added

--- Scrape_HTML.py
+++ Scrape_HTML.py
@@ -4,17 +4,26 @@ import requests
 import html2text
 import multiprocessing
 
-
 def get_url_content(link):
     """
     Return text visible on html page.
     Note: could have used unidecode(soup.text)
     """
+    h = html2text.HTML2Text()
+    h.ignore_links = True
+    h.ignore_images = True
+    h.ignore_emphasis = True
     r = requests.get(link, allow_redirects=False)
     if r.status_code == 200:
-        return h.handler(r.text)
+        return h.handle(r.text)
     else:
-        return "broken_link"
+        return "empty"
+
+
+def links_parallel(url_fn, links_lst):
+    pool = multiprocessing.Pool(4)
+    processed_links = pool.map(url_fn, links_lst)
+    return processed_links
 
 
 def main():
@@ -23,17 +32,12 @@ def main():
 
 if __name__ == '__main__':
     # main()
-    h = html2text.HTML2Text()
-    h.ignore_links = True
-    h.ignore_images = True
-    h.ignore_emphasis = True
-    file_path = '../Data/bookmarks.html'
+    file_path = 'Data/bookmarks.html'
     with open(file_path, 'r') as f:
         soup = BeautifulSoup(f, 'html.parser')
     links = [link.get('href') for link in soup.find_all('a') if
-             link.get('href').startswith('http')]
+             link.get('href').startswith('http') and not
+             link.get('href').endswith('.pdf')]
     d = {'links': links}
     df = pd.DataFrame(d)
-    pool = multiprocessing.Pool(3)
-    processed_links = pool.map(get_url_content, links)
-    # df['content'] = df['links'].apply(get_url_content)
+    processed_links = links_parallel(get_url_content, links[:30])
