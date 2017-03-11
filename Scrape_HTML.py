6e606c21289b71648f1ed5c4e0cd5091b1cddf61
bryce <bryce.luna@gmail.com>
2017-03-11 11:05:56 -0700

    cleaned code

--- Scrape_HTML.py
+++ Scrape_HTML.py
@@ -4,7 +4,6 @@ import requests
 import html2text
 import multiprocessing
 from unidecode import unidecode
-from timeit import Timer
 
 
 def html_helper(link):
@@ -14,27 +13,16 @@ def html_helper(link):
     try:
         page = requests.get(link, timeout=3.05)
     except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout,
-        requests.packages.urllib3.exceptions.LocationValueError) as e:
+           requests.packages.urllib3.exceptions.LocationValueError) as e:
         page = requests.Response()
         page.status_code = 404
-    if (not page.history and page.status_code == 200) or (page.history and page.history[0].status_code == 301):
+    if (not page.history and page.status_code == 200) or \
+       (page.history and page.history[0].status_code == 301):
         h = html2text.HTML2Text()
         h.ignore_links = True
         h.ignore_images = True
         h.ignore_emphasis = True
         return h.handle(page.text)
-    else: return "empty"
-
-
-def unidecode_helper(link):
-    """
-    Helper function to return html text.
-    Note: this is roughly 2x faster than other helper.
-    """
-    r = requests.get(link, allow_redirects=False, timeout=3.05)
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.text, 'html.parser')
-        return unidecode(soup.text)
     else:
         return "empty"
 
@@ -63,10 +51,5 @@ if __name__ == '__main__':
     d = {'links': links}
     df = pd.DataFrame(d)
     processed_links = links_parallel(html_helper, links)
-    # t1 = Timer(lambda: links_parallel(html_helper, links))
-    #processed_links = links_parallel(unidecode_helper, links)
-    #t2 = Timer(lambda: links_parallel(unidecode_helper, links))
-    # print "it took {0} for html2 helper to finish".format(t1.timeit(1))
-    #print "it took {0} for unidecode to finish".format(t2.timeit(1))
     df['text'] = processed_links
     df.to_pickle('Data/df_website_content.pkl')
